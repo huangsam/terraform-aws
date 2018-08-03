c867b42b81d68f4c9abb5ce7970008c44ba478e0
Samuel Huang <samhuang91@gmail.com>
2018-08-02 23:05:15 -0700

    Make outputs more descriptive

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,20 +1,20 @@
 output "id" {
-  description = "The ID of the VPC"
+  description = "VPC ID"
   value = "${aws_vpc.main.id}"
 }
 
 output "private_subnets" {
-  description = "The private subnet IDs"
+  description = "List of private subnet IDs"
   value = ["${aws_subnet.private_1.id}", "${aws_subnet.private_2.id}"]
 }
 
 output "public_subnets" {
-  description = "The public subnet IDs"
+  description = "List of public subnet IDs"
   value = ["${aws_subnet.public_1.id}", "${aws_subnet.public_2.id}"]
 }
 
 output "security_groups" {
-  description = "The security group IDs"
+  description = "Security group IDs by role"
   value = {
     web = "${aws_security_group.web.id}"
     app = "${aws_security_group.app.id}"
