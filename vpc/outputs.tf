b3a25b5d575fafa82ff93bcd017c730d944d0215
Samuel Huang <samhuang91@gmail.com>
2018-08-02 23:04:00 -0700

    Add security groups as output

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,14 +1,25 @@
-output "vpc_id" {
+output "id" {
   description = "The ID of the VPC"
   value = "${aws_vpc.main.id}"
 }
 
-output "vpc_private_sns" {
+output "private_subnets" {
   description = "The private subnet IDs"
   value = ["${aws_subnet.private_1.id}", "${aws_subnet.private_2.id}"]
 }
 
-output "vpc_public_sns" {
+output "public_subnets" {
   description = "The public subnet IDs"
-  value = ["${aws_subnet.private_1.id}", "${aws_subnet.private_2.id}"]
+  value = ["${aws_subnet.public_1.id}", "${aws_subnet.public_2.id}"]
+}
+
+output "security_groups" {
+  description = "The security group IDs"
+  value = {
+    web = "${aws_security_group.web.id}"
+    app = "${aws_security_group.app.id}"
+    db = "${aws_security_group.db.id}"
+    cache = "${aws_security_group.cache.id}"
+    queue = "${aws_security_group.queue.id}"
+  }
 }
